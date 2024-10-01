a01935542f37e8afae420e6daa0a4fc981abb735
Saikat Das <dassaikat1901@gmail.com>
2024-10-01 13:07:39 +0530

    reafctor auth page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,9 +1,7 @@
-import { AuthScreen } from "@/features/auth/components/auth-screen";
+import { AuthScreen } from '@/features/auth/components/auth-screen';
 
-const SignInPage = () => {
-    return (
-        <AuthScreen />
-    );
-}
- 
-export default SignInPage;
+const AuthPage = () => {
+  return <AuthScreen />;
+};
+
+export default AuthPage;
