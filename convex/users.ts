a90b51a8e6c44962b957dfcd977543cb5db4bf18
Saikat Das <dassaikat1901@gmail.com>
2024-10-01 13:06:31 +0530

    Reafactor  Users on convex

--- convex/users.ts
+++ convex/users.ts
@@ -1,58 +1,14 @@
-import { getAuthUserId } from "@convex-dev/auth/server";
-import { query } from "./_generated/server";
-import { v } from "convex/values";
-import { Id } from "./_generated/dataModel";
+import { getAuthUserId } from '@convex-dev/auth/server';
+
+import { query } from './_generated/server';
 
 export const current = query({
-    args: {},
-    handler: async (ctx, args) => {
-        const userId = await getAuthUserId(ctx);
-        if (userId === null) {
-            return null;
-        }
-        return await ctx.db.get(userId);
-    },
-});
+  args: {},
+  handler: async (ctx) => {
+    const userId = await getAuthUserId(ctx);
 
-export const getInfoById = query({
-    args: {
-        userId: v.string(),
-        workspaceId: v.id("workspaces"),
-    },
-    handler: async (ctx, args) => {
-        const userId = await getAuthUserId(ctx);
-        if (!userId) {
-            return null;
-        }
+    if (userId === null) return null;
 
-        const user = await ctx.db.get(args.userId as Id<"users">);
-
-        if (!user) {
-            return null;
-        }
-
-        const currentMember = await ctx.db
-            .query("members")
-            .withIndex("by_workspace_id_user_id", (q) => q.eq("workspaceId", args.workspaceId).eq("userId", user._id))
-            .unique();
-
-        if (!currentMember) {
-            return null;
-        }
-
-        const requestedMember = await ctx.db
-            .query("members")
-            .withIndex("by_workspace_id_user_id", (q) => q.eq("workspaceId", args.workspaceId).eq("userId", user._id))
-            .unique();
-
-        if (!requestedMember) {
-            return null;
-        }
-
-        return {
-            name: user.name,
-            email: user.email,
-            image: user.image,
-        };
-    },
+    return await ctx.db.get(userId);
+  },
 });
