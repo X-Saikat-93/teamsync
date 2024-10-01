ee1f58231382e23bf6b9e4af7f55c00bf7010a4f
Saikat Das <dassaikat1901@gmail.com>
2024-10-01 12:54:15 +0530

    Reafactor use-confirm  hook

--- src/hooks/use-confirm.tsx
+++ src/hooks/use-confirm.tsx
@@ -1,86 +1,58 @@
-import { useState } from "react"
+import { useState } from 'react';
 
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle
-} from "@/components/ui/dialog";
-import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@/components/ui/alert-dialog';
+import { Button } from '@/components/ui/button';
 
-export type ConfirmProps = {
-    title?: React.ReactNode,
-    message?: React.ReactNode,
-    proceed?: React.ReactNode,
-    cancel?: React.ReactNode,
-    urge?: boolean,
-}
+export const useConfirm = (title: string, message: string): [() => JSX.Element, () => Promise<unknown>] => {
+  const [promise, setPromise] = useState<{ resolve: (value: boolean) => void } | null>(null);
 
-export const useConfirm = (
-    title: React.ReactNode,
-    message: React.ReactNode,
-    proceed: React.ReactNode,
-    cancel: React.ReactNode,
-    urge: boolean = false,
-): [() => JSX.Element, () => Promise<unknown>] => {
-    const [promise, setPromise] = useState<{ resolve: (value: boolean) => void } | null>(null)
+  const confirm = () =>
+    new Promise((resolve) => {
+      setPromise({ resolve });
+    });
 
-    const confirm = () => new Promise((resolve, reject) => {
-        setPromise({ resolve });
-    })
+  const handleClose = () => setPromise(null);
 
-    const handleClose = () => {
-        setPromise(null);
-    }
+  const handleCancel = () => {
+    promise?.resolve(false);
 
-    const handleConfirm = () => {
-        promise?.resolve(true);
-        handleClose();
-    }
+    handleClose();
+  };
 
-    const handleCancel = () => {
-        promise?.resolve(false);
-        handleClose();
-    }
+  const handleConfirm = () => {
+    promise?.resolve(true);
 
-    const ConfirmationDialog = () => (
-        <Dialog open={promise !==null} onOpenChange={(open) => open === false && handleCancel()}>
-            <DialogContent className="md:max-w-md gap-6">
-                <DialogHeader>
-                    <DialogTitle>
-                        {title}
-                    </DialogTitle>
-                    {message && (
-                        <DialogDescription>
-                            {message}
-                        </DialogDescription>
-                    )}
-                </DialogHeader>
-                <DialogFooter
-                    className="flex-row *:flex-1 gap-2"
-                >
-                    <Button
-                        variant={urge ? "ghost" : "default"}
-                        onClick={handleCancel}
-                    >
-                        {cancel}
-                    </Button>
-                    <Button
-                        variant={!urge ? "ghost" : "default"}
-                        className={cn(
-                            !urge && "!text-red-700 hover:bg-red-100"
-                        )}
-                        onClick={handleConfirm}
-                    >
-                        {proceed}
-                    </Button>
-                </DialogFooter>
-            </DialogContent>
-        </Dialog>
-    )
+    handleClose();
+  };
 
-    return [ConfirmationDialog, confirm]
-}
+  const ConfirmDialog = () => (
+    <AlertDialog open={promise !== null}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+
+          <AlertDialogDescription>{message}</AlertDialogDescription>
+        </AlertDialogHeader>
+
+        <AlertDialogFooter className="pt-2">
+          <Button onClick={handleCancel} variant="outline">
+            Cancel
+          </Button>
+
+          <Button onClick={handleConfirm} variant="destructive">
+            Confirm
+          </Button>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+
+  return [ConfirmDialog, confirm];
+};
