16e67353be1a932fc21eac91fa4e7c9761bb94eb
Saikat Das <dassaikat1901@gmail.com>
2024-10-01 13:01:25 +0530

    refactor convex auth

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,8 +1,9 @@
-import GitHub from "@auth/core/providers/github";
-import Google from "@auth/core/providers/google";
-import { convexAuth } from "@convex-dev/auth/server";
-import { Password } from "@convex-dev/auth/providers/Password";
-import { DataModel } from "./_generated/dataModel";
+import GitHub from '@auth/core/providers/github';
+import Google from '@auth/core/providers/google';
+import { Password } from '@convex-dev/auth/providers/Password';
+import { convexAuth } from '@convex-dev/auth/server';
+
+import { DataModel } from './_generated/dataModel';
 
 const CustomPassword = Password<DataModel>({
   profile(params) {
@@ -14,5 +15,5 @@ const CustomPassword = Password<DataModel>({
 });
 
 export const { auth, signIn, signOut, store } = convexAuth({
-  providers: [ CustomPassword],
+  providers: [CustomPassword, GitHub, Google],
 });
