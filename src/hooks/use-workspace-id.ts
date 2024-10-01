d1b1fffdb25fc15e5a4ffe87f94796ce890bd07f
Saikat Das <dassaikat1901@gmail.com>
2024-10-01 12:53:17 +0530

    add custom hooks

--- src/hooks/use-workspace-id.ts
+++ src/hooks/use-workspace-id.ts
@@ -1,8 +1,15 @@
-import { useParams } from "next/navigation"
-import { Id } from "../../convex/_generated/dataModel";
+'use client';
+
+import { useParams } from 'next/navigation';
+
+import type { Id } from '../../convex/_generated/dataModel';
+
+type WorkspaceIdParams = {
+  workspaceId: Id<'workspaces'>;
+};
 
 export const useWorkspaceId = () => {
-    const params = useParams();
+  const params = useParams<WorkspaceIdParams>();
 
-    return params.workspaceId as Id<"workspaces">;
-}
+  return params.workspaceId;
+};
